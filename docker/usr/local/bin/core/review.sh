30028dffe35dbf75fe6dad6aa149b889b0c034ca
storezhang <storezhang@gmail.com>
2026-05-26 15:01:51 +0800

    feat(审核): 使用sed过滤注释和空行

--- docker/usr/local/bin/core/review.sh
+++ docker/usr/local/bin/core/review.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 workdir=${WORKDIR=${CI_WORKDIR=.}}
+remote=${REMOTE=${CI_REMOTE=origin}}
 source=${SOURCE=${CI_SOURCE=main}}
 target=${TARGET=${CI_TARGET=main}}
+
+from="${remote}/${source}"
+to="${remote}/${target}"
 output_file="$1"
 output_format="$2"
 settings="$3"
@@ -20,31 +24,24 @@ cd "${workdir}" || exit 1
 if ! git rev-parse --git-dir > /dev/null 2>&1; then
     log warn 非代码仓库目录 "dir=$(pwd)"
 else
-    log debug 切换分支 "source=${source},target=${target}"
-    if ! git switch "${target}" > /dev/null 2>&1; then
+    log debug 切换分支 "source=${from},target=${to}"
+    if ! git switch "${to}" > /dev/null 2>&1; then
         log warn 切换分支失败 "target=${target}"
     fi
 fi
 
 # 检查是否相同
-if [ "${source}" = "${target}" ]; then
-    log info 使用全量安全审核模式 "source=${source},target=${target}"
+if [ "${from}" = "${to}" ]; then
+    log info 使用全量安全审核模式 "source=${from},target=${to}"
     diff="$(git rev-list --max-parents=0 HEAD)"
 else
-    log info 使用差异安全审核模式 "source=${source},target=${target}"
-
-    # 确保分支存在
-    if ! git fetch origin "${source}" > /dev/null 2>&1; then
-        log warn 获取分支失败 "source=${source}"
-    fi
-    diff="${source}..${target}"
+    log info 使用差异安全审核模式 "source=${from},target=${to}"
+    diff="${from}...${to}"
 fi
-
-# l
 
 prompt=$(cat "/opt/dockerat/prompt/security-review.md")
 log info 安全审核开始 "dir=$(pwd)"
-git diff "${diff}" | claude --print --output-format="${output_format}" --settings "${settings}" security-review "${prompt}" > "${output_file}"
+git diff "${diff}" | sed '/^+[[:space:]]*\/\//d' | sed '/^+[[:space:]]*$/d' | claude --print --output-format="${output_format}" --settings "${settings}" security-review "${prompt}" > "${output_file}"
 log info 安全审核完成 "dir=$(pwd),filename=${output_file}"
 
 # 清理过程文件
